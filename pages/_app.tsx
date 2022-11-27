d1b52f882ca86100876ad046cfa21e4da3239726
hartaithan <hartaithan@gmail.com>
2022-11-27 13:37:40 +0900

    install font via next optimization tools

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Lora } from "@next/font/google";
+
+const lora = Lora({
+  subsets: ["latin", "cyrillic"],
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lora.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default App;
