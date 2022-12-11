a505bb0fe2f351216138491b65ac882e0bb387f1
hartaithan <hartaithan@gmail.com>
2022-12-11 12:42:04 +0900

    move head in layouts

--- locales/main.ts
+++ locales/main.ts
@@ -3,11 +3,13 @@ import { ILocaleObject } from "../models/LocaleModel";
 export const main: ILocaleObject = {
   en: {
     title: "Journal 29 Hints",
+    admin: "Admin Panel",
     welcome: "Welcome to",
     locale: "Locale:",
   },
   ru: {
     title: "Дневник 29 Подсказки",
+    admin: "Админ. панель",
     welcome: "Добро пожаловать в",
     locale: "Локаль:",
   },
