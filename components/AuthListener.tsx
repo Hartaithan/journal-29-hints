70349f350a00c60ceb2825c0a4e6c5c6e7638dd5
hartaithan <hartaithan@gmail.com>
2022-12-17 13:19:12 +0900

    add auth listener

---
+++ components/AuthListener.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FC, useEffect } from "react";
+import { supabase } from "../helpers/supabase";
+
+interface IAuthListenerProps {
+  accessToken?: string;
+}
+
+const AuthListener: FC<IAuthListenerProps> = (props) => {
+  const { accessToken } = props;
+  const router = useRouter();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== accessToken) {
+        router.refresh();
+      }
+    });
+  }, [accessToken]); // eslint-disable-line
+
+  return null;
+};
+
+export default AuthListener;
