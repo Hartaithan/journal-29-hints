483475888cb3c15a3bf01590cf421d7350fd713d
hartaithan <hartaithan@gmail.com>
2023-01-06 15:49:41 +0900

    get book data on admin/books/[id]

--- pages/admin/books/[id]/index.tsx
+++ pages/admin/books/[id]/index.tsx
@@ -1,11 +1,57 @@
+import { createServerSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Flex from "../../../../components/Flex";
 import AdminLayout from "../../../../layouts/AdminLayout";
-import { NextPageWithLayout } from "../../../../models/AppModel";
+import { IPageProps, NextPageWithLayout } from "../../../../models/AppModel";
+import { IBook } from "../../../../models/BookModel";
 
-const AdminBookPage: NextPageWithLayout = () => {
+interface IAdminBookPageProps extends IPageProps {
+  book: IBook | null;
+}
+
+export const getServerSideProps: GetServerSideProps<
+  IAdminBookPageProps
+> = async (ctx) => {
+  const id = ctx.params?.id;
+  const supabase = createServerSupabaseClient(ctx);
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+  const { data: book, error } = await supabase
+    .from("books")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/admin/signIn",
+        permanent: false,
+      },
+    };
+  }
+
+  if (error) {
+    console.error("get book error", error);
+  }
+
+  return {
+    props: {
+      initialSession: session,
+      user: session.user,
+      book: book || null,
+    },
+  };
+};
+
+const AdminBookPage: NextPageWithLayout<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> = (props) => {
+  const { book } = props;
   return (
     <Flex direction="column" justify="center" align="center">
-      AdminBookPage
+      {JSON.stringify(book, null, 2)}
     </Flex>
   );
 };
